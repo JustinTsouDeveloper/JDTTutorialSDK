e2ff542fe34885f19ae7608eccf634cbf5c73ecb
JustinTsou <killkill8250@gmail.com>
2021-04-17 16:38:21 +0800

    Add nib file

---
+++ JDTTutorialSDK/TestLoadNibView.swift
@@ -0,0 +1,40 @@
+//
+//  TestLoadNibView.swift
+//  JDTTutorialSDK
+//
+//  Created by Justin Tsou on 2021/4/17.
+//
+
+import UIKit
+
+class TestLoadNibView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public convenience init(text:String, frame:CGRect) {
+        self.init(frame: frame)
+        self.setupTestLoadNibView(frame:frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupTestLoadNibView(frame:CGRect) {
+        
+        let path = Bundle.main.resourcePath! + "/JDTTutorialSDKPods.bundle/JDTBundle.bundle"
+        let resource_bundle = Bundle(path:path)
+
+        let nib = UINib(nibName: "TutorialView", bundle: resource_bundle)
+
+        contentView = nib.instantiate(withOwner: self, options: nil).first as? UIView
+        contentView.frame = bounds
+
+        addSubview(contentView)
+    }
+}
